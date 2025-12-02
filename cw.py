4e63fc0b0a99e0c0f149ecf9a167fc231835b737
Tapasi <33500374+tapasi56@users.noreply.github.com>
2025-12-02 18:49:48 +0000

    Add files via upload

--- cw.py
+++ cw.py
@@ -14,24 +14,12 @@ class Circle():
         self.circle_screen = screen
     
     def draw(self):
-        self.draw_circle = pygame.draw.circle()
-pygame.init()
-screen = pygame.display.set_mode((600, 800))
-blue = (0,0,250)
-green = (0,250,0)
-
-class Circle():
-    def __init__(self,color,pos,radius,width):
-        self.circle_color = color
-        self.circle_pos = pos
-        self.circle_radius = radius
-        self.circle_width = width
-        self.circle_screen = screen
-    
-    def draw(self):
         self.draw_circle = pygame.draw.circle(self.circle_screen, self.circle_color ,self.circle_pos ,self.circle_radius ,self.circle_width )
 
-
+    def grow(self, r):
+        self.circle_radius = self.circle_radius + r
+        self.draw_circle = pygame.draw.circle(self.circle_screen, self.circle_color ,self.circle_pos ,self.circle_radius ,self.circle_width )
+        
 bcircle = Circle(blue, (300,300), 25, 0)
 gcircle = Circle(green, (100,100), 25, 2)
 while True:
@@ -41,6 +29,7 @@ while True:
             bcircle.draw()
             gcircle.draw()
             pygame.display.update()
+        
         
 
 
